69d55f2cde6b99ef3948c15b6464c124581e7220
RJ_Infinity <rjinfinity2004@gmail.com>
2022-06-30 20:04:46 +0100

    added Colour

--- src/Structs.cs
+++ src/Structs.cs
@@ -10,11 +10,8 @@ namespace SDL_Sharp
     public struct Rect
     {
         public int x;
-
         public int y;
-
         public int w;
-
         public int h;
         public static explicit operator Rect(SDL.SDL_Rect r)
         {
@@ -33,6 +30,24 @@ namespace SDL_Sharp
             rv.w = r.w;
             rv.h = r.h;
             return rv;
+        }
+    }
+    public struct Colour
+    {
+        public byte r;
+        public byte g;
+        public byte b;
+        public byte a;
+        public static explicit operator uint(Colour c) =>
+            (uint) (c.r | (c.g << 8) | (c.b << 16) | (c.a << 24));
+        public static explicit operator Colour(uint n)
+        {
+            Colour c = new Colour();
+            c.r =(byte)n;
+            c.g = (byte)(n >> 8);
+            c.b = (byte)(n >> 16);
+            c.a = (byte)(n >> 24);
+            return c;
         }
     }
 }
